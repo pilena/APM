a394734b11b669102b991e095f409703c8be38a8
pilena24 <lenkazivkovic24@gmail.com>
2020-11-30 13:13:32 +0100

    building custom pipe from replacing all special characters to spaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
+import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule, 
-    FormsModule
+    FormsModule, 
+   
   ],
   bootstrap: [AppComponent]
 })
