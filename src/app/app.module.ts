23a8e2cae72b735a7b36caac9c06d60b28bf832a
pilena24 <lenkazivkovic24@gmail.com>
2020-12-01 13:10:25 +0100

    adding and nesting star.component for adding star rating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
-import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 // don't forget to import all components and pipes to angular module
 // since we don't have more modules, everything goes to app module
@@ -13,7 +14,8 @@ import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
     // *here we write directives, components and pipes we wrote ourselves 
     AppComponent,
     ProductListComponent, 
-    ConvertToSpacesPipe
+    ConvertToSpacesPipe, 
+    StarComponent
   ],
   imports: [
     // *here we import ones already written for us 
