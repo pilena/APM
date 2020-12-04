885580d084ed327cc7063cf1c4107d96ef0175e6
pilena24 <lenkazivkovic24@gmail.com>
2020-12-04 13:16:11 +0100

    setting up for sending an http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
@@ -23,7 +24,7 @@ import { StarComponent } from './shared/star.component';
     // *here we import ones already written for us 
     BrowserModule, 
     FormsModule, 
-   
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
